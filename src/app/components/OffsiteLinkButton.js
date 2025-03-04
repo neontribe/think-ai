291dd82a46fb2b73b574460f37ecc5e260762c40
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-04 10:53:30 +0000

    Install Tailwind package

---
+++ src/app/components/OffsiteLinkButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  OffsiteLinkButtons = ({href, children, onclick}) => {
+    return (
+        href={href}
+        onclick={onclick}
+        className={}
+        
+
+    )
+}
+
