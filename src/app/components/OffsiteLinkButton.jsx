735d8315e468a9acb7ba791ae48b4427e3d7a99d
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-04 17:08:33 +0000

    Add style to className

--- src/app/components/OffsiteLinkButton.jsx
+++ src/app/components/OffsiteLinkButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const  OffsiteLinkButton = ({href, children, onClick, className}) => {
+const  OffsiteLinkButton = ({href, children, onClick, className = ""}) => {
     return (
         <a
         href={href}
         target="_blank"
         rel="noopener noreferrer"
         onClick={onClick}
-        className={`inline-block rounded-full bg-blue-600 px-6 py-2 text-white font-semibold hover:bg-blue-400 focus:outline-none ${className}`}
+        className={`inline-flex items-center justify-center px-6 py-3 text-lg font-bold rounded-full transition durantion-300 bg-[#63E2F1] text-[#1B0080] hover:bg-[#9747FF] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring[#1B0080] ${className}`}
         >
             {children}
         </a>
