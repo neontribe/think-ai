fcb5e78fd8472f19da9f1991ec3d84809cc267c9
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-06 15:36:19 +0000

    Feat: Create caroulse skeleton

---
+++ src/app/components/ResponseCarousel.jsx
@@ -0,0 +1,17 @@
+"use clinet"
+
+import React, { useState } from "react";
+
+const ResponseCarousel = ({ slideData}) => {
+    const[currentSlide, setCurrentSlide] = useState(0);
+
+
+    return (
+        <div className="relative w-full max-w-4xl h-72 md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl overflow-hidden">
+            
+        </div>
+    );
+};
+
+export default ResponseCarousel;
+
