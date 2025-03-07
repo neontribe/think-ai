f123e3f3a0d406fb399e90592307b348f39bb7a1
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-07 10:54:54 +0000

    Add z-20 to ensure the buttons are above the slides

--- src/app/components/ResponseCarousel.jsx
+++ src/app/components/ResponseCarousel.jsx
@@ -3,8 +3,19 @@
 import React, { useState } from "react";
 import ArrowButton from "./ArrowButton";
 
-const ResponseCarousel = ({ slideData = [] }) => {
+const ResponseCarousel = () => {
     const [currentSlide, setCurrentSlide] = useState();
+
+    const slideData = [
+        {
+            number: "1.",
+            paragraph: "This is the first slide"
+        },
+        {
+            number: "2.",
+            paragraph: "This is the second slide"
+        },
+    ];
 
     const nextSlide = () => {
         setCurrentSlide((prev) => (prev -1 + slideData.length) % slideData.lenght);
@@ -17,29 +28,30 @@ const ResponseCarousel = ({ slideData = [] }) => {
 
     return (
     <>
+        {/* outer container */}
         <div className="relative w-full max-w-4xl h-72 md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl overflow-hidden mx-auto">
             {/* slider container */}
-            <div className="relative w-full h-full flex transition-transform duration-500">
+            <div className="relative w-full h-full flex">
                 {slideData.map((slide, index) => (
                     <div
                         key={index}
-                        className={`absolute inset-0 flex items-center justify-center text-white transition-opacity duration-500 ${index === currentSlide ? "opacity-100" : "opacity-0"}`}
+                        className={`absolute inset-0 flex items-center justify-center text-white z-10 transition-opacity duration-500 ${index === currentSlide ? "opacity-100" : "opacity-0"}`}
                     >
                         {/* slide content */}
-                        <div className="p-8 bg-blue-700/80 rounded-3xl max-w-[90%]">
-                            <h1 className="text-3xl font-bold">{slide.paragraph}</h1>
+                        <div className="p-8 bg-blue-700/80 rounded-3xl max-w-[90%] flex">
+                        <div className="text-5xl font-bold mr-4">{slide.number}</div>
+                        <div className="flex-1">
+                            <h1 className="mt-4 text-lg font-bold">{slide.paragraph}</h1>
+                            </div>
                         </div>
                     </div>
                 ))}
+                <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] bg-red-500 px-4 z-20">
+                    <ArrowButton direction="left" onClick={nextSlide}  ariaLabel="Next slide"/>
+                    <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide"/>
+                </div>
             </div>
         </div>
-
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] bg-red-500 px-4">
-        <ArrowButton direction="left" onClick={nextSlide}  ariaLabel="Next slide"/>
-        <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide" />
-        </div>
-
-
         </>
     );
     
