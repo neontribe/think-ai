8d7c8b5c8e53e9dc22a97c89c38ceafbdb15a7f4
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-07 16:20:09 +0000

    Fix: style

--- src/app/components/ResponseCarousel.jsx
+++ src/app/components/ResponseCarousel.jsx
@@ -7,11 +7,11 @@ const ResponseCarousel = () => {
     const slideData = [
         {
             number: "1.",
-            paragraph: "This is the first slide"
+            paragraph: "This is the first slide.is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages "
         },
         {
             number: "2.",
-            paragraph: "This is the second slide"
+            paragraph: "This is the second slide. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages"
         },
     ];
 
@@ -48,9 +48,9 @@ const nextSlide = () => {
     return (
     <>
         {/* outer container */}
-        <div className="relative w-full max-w-4xl h-72 md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl overflow-hidden mx-auto">
+        <div className="relative w-full max-w-4xl md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl shadwo-lg overflow-hidden mx-auto">
             {/* slider container */}
-            <div className="relative w-full h-full flex">
+            <div className="flex flex-col items-startspace-y-4 ">
                 {slideData.map((slide, index) => {
                     const isActive = index === currentIndex;
                     return (
@@ -61,16 +61,16 @@ const nextSlide = () => {
                             ${isActive ? "opacity-100 z-10" : "opacity-0 z-0"}`}
                     >
                         {/* slide content */}
-                        <div className="p-8 bg-blue-700/80 rounded-3xl max-w-[90%] flex">
-                        <div className="text-5xl font-bold mr-4">{slide.number}</div>
+                        <div className="flex items-start self-start -mt-2 p-14 mr-4 rounded-3xl max-w-[90%] flex">
+                        <div className="text-8xl font-bold relative mt-8 mr-4">{slide.number}</div>
                         <div className="flex-1">
-                            <h1 className="mt-4 text-lg font-bold">{slide.paragraph}</h1>
+                            <h1 className="text-lg">{slide.paragraph}</h1>
                             </div>
                         </div>
                     </div>
                     )
                     })}
-                <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] bg-red-500 px-4 z-20">
+                <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] px-4 z-20">
                     <ArrowButton direction="left" onClick={nextSlide}  ariaLabel="Next slide"/>
                     <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide"/>
                 </div>
