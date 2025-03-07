e1aa0a8c663e92f71a7efaa639c4560619aa4b64
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-07 18:06:37 +0000

    Fix slide navigation

--- src/app/components/ResponseCarousel.jsx
+++ src/app/components/ResponseCarousel.jsx
@@ -3,24 +3,31 @@
 import React, { useState } from "react";
 import ArrowButton from "./ArrowButton";
 
-const ResponseCarousel = ({slideData = [] }) => {
+const ResponseCarousel = () => {
+    const slideData = [ 
+        {number: "1", paragraph: "This is a placeholder slide number one."},
+        {number: "2", paragraph: "This is a placeholder slide number two."},
+        {number: "3", paragraph: "This is a placeholder slide number three."},
+        
+    ]
+    const [localSlides] = useState(slideData);
     const [currentIndex, setCurrentIndex] = useState(0)
 
     const updateSlide = (newIndex) => {
         setCurrentIndex(newIndex);
     };
     
-    const prevSlide = () => {
+    const nextSlide = () => {
         let newIndex = currentIndex-1;
         if (newIndex < 0) {
-            newIndex = slideData.length -1;
+            newIndex = localSlides.length -1;
         }
         return updateSlide(newIndex)
     }
     
-    const nextSlide = () => {
+    const prevSlide = () => {
         let newIndex = currentIndex+1;
-        if (newIndex == slideData.length) {
+        if (newIndex == localSlides.length) {
             newIndex = 0;
         }
         return updateSlide(newIndex)
@@ -29,40 +36,39 @@ const ResponseCarousel = ({slideData = [] }) => {
 
 
     return (
-    <>
-        {/* outer container */}
-        <div className="relative w-full max-w-4xl md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl shadwo-lg overflow-hidden mx-auto">
-            {/* slider container */}
-            <div className="flex flex-col items-startspace-y-4 ">
-                {slideData.map((slide, index) => {
-                    const isActive = index === currentIndex;
-                    return (
-                    
-                    <div 
-                        key={index}
-                        className={`absolute  bg-green inset-0 flex items-center justify-center text-white transition-opacity duration-500 
-                            ${isActive ? "opacity-100 z-10" : "opacity-0 z-0"}`}
-                    >
-                        {/* slide content */}
-                        <div className="flex items-start self-start -mt-2 p-14 mr-4 rounded-3xl max-w-[90%] flex">
-                        <div className="text-8xl font-bold relative mt-8 mr-4">{slide.number}</div>
-                        <div className="flex-1">
-                            <h1 className="text-lg">{slide.paragraph}</h1>
+        <>
+            {/* outer container */}
+            <div className="relative w-full max-w-4xl md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl shadwo-lg overflow-hidden mx-auto">
+                {/* slider container */}
+                <div className="flex flex-col items-startspace-y-4 ">
+                    {slideData.map((slide, index) => {
+                        const isActive = index === currentIndex;
+                        return (
+                        
+                        <div 
+                            key={index}
+                            className={`absolute  bg-green inset-0 flex items-center justify-center text-white transition-opacity duration-500 
+                                ${isActive ? "opacity-100 z-10" : "opacity-0 z-0"}`}
+                        >
+                            {/* slide content */}
+                            <div className="flex items-start self-start -mt-2 p-14 mr-4 rounded-3xl max-w-[90%] flex">
+                            <div className="text-8xl font-bold relative mt-8 mr-4">{slide.number}</div>
+                            <div className="flex-1">
+                                <h1 className="text-lg">{slide.paragraph}</h1>
+                                </div>
                             </div>
                         </div>
+                        )
+                        })}
+                    <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] px-4 z-20">
+                        <ArrowButton direction="left" onClick={nextSlide}  ariaLabel="Next slide"/>
+                        <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide"/>
                     </div>
-                    )
-                    })}
-                <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] px-4 z-20">
-                    <ArrowButton direction="left" onClick={nextSlide}  ariaLabel="Next slide"/>
-                    <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide"/>
                 </div>
             </div>
-        </div>
-        </>
-    );
+            </>
+        );
+        
+    };
     
-};
-
-export default ResponseCarousel;
-
+    export default ResponseCarousel;
