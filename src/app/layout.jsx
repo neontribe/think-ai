15d62ae8c637b8f923905554fd24ac228ac255b7
charles strange <charles@neontribe.co.uk>
2025-03-10 16:06:42 +0000

    feat: add global poppins font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,13 @@
 import './globals.css'
 import {GlobalStateProvider} from "@/app/contexts/GlobalStateProvider";
 import Link from "next/link";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Think AI",
@@ -12,8 +19,8 @@ export const metadata = {
 /* creates the default root layout */
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="font-mono bg-blue-700 text-gray-100">
+    <html lang="en" className={poppins.className}>
+      <body className="bg-blue-700 text-gray-100">
         <GlobalStateProvider>
           <header className="p-2">
             <Link href="/" className="text-3xl">Think AI</Link>
