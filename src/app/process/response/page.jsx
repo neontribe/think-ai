05da09a3af7be0bbe8ef2bbd0f8d58d588f3b6e0
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-11 13:20:09 +0000

    Create basic 2-col layout and text breakpoint

--- src/app/process/response/page.jsx
+++ src/app/process/response/page.jsx
@@ -14,6 +14,36 @@ const Response = () => {
     <button className="bg-[#63E2F1] text-[#1B0080] px-6 py-3 w-[171px] h-[48px] text-lg inline-flex items-center justify-center font-bold rounded-full transition durantion-300 hover:bg-[#9747FF] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#9747FF]" type="button" onClick={() => router.push('/process/discuss')}>
       Find Out
     </button>
+    {/* whole screen size */}
+    <div class="bg-gray-400 flex justyfy-center items-center min-h-screen">
+      
+      <div class=" container mx-auto w-[1150px] h-[466px] bg-gray-700 flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 h-full bg-blue-500 text-white flex flex-col items-center justify-center md:order-1 3xl:order-2">
+          <h1>Column 1</h1>
+        </div>
+        <div className="w-full md:w-1/2 h-full bg-green-700 text-white flex flex-col items-center justify-center md:oder-2 3xl:order-1">
+          <h1>Column 2</h1>
+        </div>
+        
+      </div>
+
+
+      {/* <div class="bg-gray-400 flex justyfy-center items-center min-h-screen">
+        
+        <div className= "w-[1150px] h-[466px] bg-gray-700 flex flex-col md:flex-row">
+          
+          <div className=" w-full md:w-1/2 h-full bg-blue-500 text-white flex flex-col items-center justify-center md:order-2 order-1">
+          <h1>Column 2</h1>
+          </div>
+          
+          <div className="w-full md:w-1/2 h-full bg-green-700 text-white flex flex-col items-center justify-center md:order-1 order-2">
+          <h1>Column 1</h1>
+          </div>
+        </div>
+      </div> */}
+      
+
+    </div>
   </>);
 }
 
