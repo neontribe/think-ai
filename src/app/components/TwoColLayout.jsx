e2eb96b71ce1ef769cc81ada197d0e2d8f3afead
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-11 16:08:12 +0000

    Add children prop

--- src/app/components/TwoColLayout.jsx
+++ src/app/components/TwoColLayout.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 const TwoColLayout=({
+    children,
     leftColumn,
     rightColumn,
     containerClass = "w-full md:w-[1150px] h-auto md:h-[466px]",
@@ -10,10 +11,10 @@ const TwoColLayout=({
     return (
         <div className={` ${containerClass} bg-[#1C0080] flex mx-auto md:flex-row`}>
             <div className="text-white flex flex-col items-center justify-center md:order-1 3xl:order-2">
-            {leftColumn}
+            {leftColumn || children}
             </div>
             <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:oder-2 3xl:order-1">
-            {rightColumn}
+            {rightColumn || children}
             </div>
         </div>
     )
