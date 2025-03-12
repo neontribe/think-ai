cc1318f3946c479db494a080e51e4d86d8eb497d
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-12 11:44:24 +0000

    Add navigation Elements

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,19 +3,23 @@
 import React from "react";
 import Link from 'next/link';
 
-const Header = () => {
+const Header = ({showNav = true}) => {
     return (
         <header className="w-full bg-[#1C0080] text-white py-4 px-6 flex justify-between items-center">
-            <div>
-                <Link href="/">
-                <h1 className="text-3xl font-semibold cursor-pointer">Real Talk <spam className="text-[#87E2D9]">AI</spam></h1>
-                </Link>
-
-                <div>
-                    {/* button block goes here */}
-
+            <Link href="/">
+            <h1 className="text-3xl font-semibold cursor-pointer">Real Talk <spam className="text-[#87E2D9]">AI</spam></h1>
+            </Link>
+            {/* Link and button block - can be hidden via prop */}
+            {showNav && (
+                <div className="flex gap-8 items-center px-4 py-2">
+                    <Link href="/" className="text-lg underline">our team</Link>
+                    <Link href="/"> 
+                    <button className="px-4 py-2 text-lg text-white bg-[#1C0080] border-2 border-[#65E1F1] rounded-full cursor-pointer">for group leaders</button>
+                    </Link>
                 </div>
-            </div>
+
+            )}
+            
         </header>
     )
 }
