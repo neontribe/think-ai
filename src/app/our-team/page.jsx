3901166eb90e8ffb33929e93a6a0c7c54c4be11c
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-13 19:54:04 +0000

    Add useEffect to log hidelinks prop value in Header

--- src/app/our-team/page.jsx
+++ src/app/our-team/page.jsx
@@ -1,14 +1,13 @@
 'use client';
+
 import Header from "../components/Header";
 
-
-export default function GroupLeadersPage() {
+export default function OurTeamPage() {
     return (
         <div>
-            <header hidelinks ={true}/>
-            <h1>OUT TEAM PAGE</h1>
+            <Header hidelinks={true} />
+            <h1>OUR TEAM PAGE</h1>
             <p>info page</p>
         </div>     
-       
-      );
-  };
+    );
+}
