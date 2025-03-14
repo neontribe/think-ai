25333642f50c8beab1803e899cd9cc9d7cbdf0e4
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-14 13:39:41 +0000

    Refactor: add imageModel

--- src/app/components/PromptInput.jsx
+++ src/app/components/PromptInput.jsx
@@ -14,11 +14,14 @@ export default function PromptInput({ apiEndpoint, suggestedText, onSubmit}) {
         setPromptSubmitted(true);
         setErrorMessage("");
     
-      try {
-        const response = await fetch(apiEndpoint, {
-          method: "POST",
-          body: JSON.stringify({ prompt }),
-        });
+        try {
+            const response = await fetch(apiEndpoint, {
+              method: "POST",
+              body:
+                modelType === "image-generation"
+                  ? JSON.stringify({ prompt, imageModel })
+                  : JSON.stringify({ prompt }),
+            });
 
         if(!response.ok) {
             throw new Error("Network reposnse was not ok");
@@ -35,8 +38,27 @@ export default function PromptInput({ apiEndpoint, suggestedText, onSubmit}) {
     }
 
     return (
+        <div className="w-full max-w-lg p-8 bg-white rounded-2xl shadow-md flex felc-col">
+            <form onSubmit={handleSubmit}>
+                <div className="relative">
+                    <textarea
+                    id="prompt"
+                    className="w-full p-4 pr-12 rounded-2xl text-gray-700 placeholder-gray-500 px-6 py-3 w-[171px] h-[48px] text-lg inline-flex items-center justify-center"
+                    placeholder={suggestedText}
+                    maxLength={10000}
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    ></textarea>
+                    <div className="absolute right-4 top-1/2 transform-translate-y-1/2">
+                    <ArrowButton />
+                    </div>
+                </div> 
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+            </form>
+        </div>
 
-    )
-}
+    );
+};
+
 
 
