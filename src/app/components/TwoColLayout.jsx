cbf4c7508efd2c8ddc8d36a5c5bf2638b1faf2fe
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-14 10:51:18 +0000

    Refactor TwoColLayout component

--- src/app/components/TwoColLayout.jsx
+++ src/app/components/TwoColLayout.jsx
@@ -1,22 +1,20 @@
 "use client"
 
 import React from "react";
-import ContentBox from '@/app/components/ContentBox';
 
 const TwoColLayout=({
+    children,
     leftColumn,
     rightColumn,
-    containerClass = "w-full md:w-[1150px] h-auto md:min-h-[466px]",
+    containerClass = "w-full md:w-[1150px] h-auto md:h-[466px]",
 }) => {
     return (
-        <div className={` ${containerClass} bg-[#1C0080] flex p-2 mx-auto md:flex-row`}>
-            <div className="text-white flex flex-col items-center justify-center md:order-1 3xl:order-2">
-            {leftColumn}
+        <div className={` ${containerClass} bg-[#1C0080] flex mx-auto md:flex-row`}>
+            <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-1 3xl:order-2">
+            {leftColumn || children}
             </div>
             <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:oder-2 3xl:order-1">
-            {rightColumn}
-            <ContentBox>
-            </ContentBox>
+            {rightColumn || children}
             </div>
         </div>
     )
@@ -24,7 +22,5 @@ const TwoColLayout=({
 }
 
 export default TwoColLayout;
-
-
 
 
