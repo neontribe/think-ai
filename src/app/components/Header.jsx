c116fc7a4995279e7fdffefb34cff67da74ebb59
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-14 09:21:03 +0000

    Fix style

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ hidelinks = false }) => {
     return (
         <header role="banner" className="w-full bg-[#1C0080] text-white py-4 px-6 flex justify-between items-center">
             <Link href="/" className="focus:outline-none focus:ring-2 focus:ring-[#65E1F1]">
-                <h1 className="text-3xl font-semibold cursor-pointer" id="site-title">
+                <h1 className="text-[28px] font-semibold cursor-pointer" id="site-title">
                     <span className="block md:inline">Real Talk</span> 
                     <span className="text-[#87E2D9] md:ml-2">AI</span>
                 </h1>
@@ -20,9 +20,9 @@ const Header = ({ hidelinks = false }) => {
             {/* Conditional rendering based on hidelinks prop */}
             {!hidelinks && (
                 <nav className="flex md:flex-row flex-col gap-2 md:gap-8 items-end" aria-label="Main navigation">
-                    <Link href="/our-team" className="text-lg underline mb-2 md:mb-0 focus:outline-none focus:ring-[#65E1F1] rounded-md" aria-label="View our team">our team</Link>  
-                    <Link href="/group-leaders" className="w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-[#65E1F1]" aria-label="resources for group leaders"> 
-                        <button className="px-4 py-2 text-lg text-white bg-[#1C0080] border-2 border-[#65E1F1] rounded-full cursor-pointer" type="button">for group leaders</button>
+                    <Link href="/our-team" className="text-[16px] hover:text-[#87E2D9] underline mb-2 md:mb-0 focus:outline-none focus:ring rounded-md" aria-label="View our team">our team</Link>  
+                    <Link href="/group-leaders" className="w-full md:w-auto focus:outline-none focus:ring-2 focus:ring" aria-label="resources for group leaders"> 
+                        <button className="px-4 py-2 text-[20px] text-white bg-[#1C0080] border-2 border-[#65E1F1] rounded-full cursor-pointer hover:text-[#87E2D9]" type="button">for group leaders</button>
                     </Link>    
                 </nav>
             )}
