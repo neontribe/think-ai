848694a2f92231c304a0e18f0ae4a0b69c7df555
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-18 15:38:41 +0000

    Feat: add ButTypography component

---
+++ src/app/components/ButTypography.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+
+const ButTypography = ({ text = '' , className = '' }) => {
+  return (
+    <div
+    className={`flex flex-row justify-center items-baseline bg-transparent text-[#65E1F1] text-[3.75rem] ${className} 
+    w-full max-w-lg md:max-w-xl lg:max-w-4xl h-20 md:h-24 lg:h-[28rem]`}
+      aria-labelledby='but-text'
+    >
+      <span className='block mr-4 font-bold' tabIndex='0'>BUT</span>
+      {text && <p className='text-lg text-white text-[18px] top-1' tabIndex='0'>{text}</p>}
+    </div>
+  );
+};
+
+export default ButTypography;
+  
