0a4d04556f4f82b0088bde21d3f0456247a093bb
charles strange <charles@neontribe.co.uk>
2025-03-19 10:58:42 +0000

    refactor: simplify

--- src/app/components/CustomImage.jsx
+++ src/app/components/CustomImage.jsx
@@ -18,56 +18,48 @@ const CustomImage = ({
   overlayText = '',
 }) => {
 
+  const commonImage = (<Image
+    src={src}
+    alt={alt}
+    width={!fill ? width : undefined}
+    height={!fill ? width : undefined}
+    fill={fill}
+    sizes={sizes}
+    style= { {borderRadius, objectFit: 'cover', ...style }}
+    className={className}
+    tabIndex='0'
+  />);
+
   return (
     <figure className='relative mx-auto text-center w-full max-w-[75rem]'>
-      {/* if link not provided, the image should no be wrapped in a <a> tag */}
+      {/* if link not provided, the image should not be wrapped in a <a> tag */}
       { link ? (
          <a
-         href={link || '#'} 
+         href={link || '#'}
          target='_blank'
          rel='noopener noreferrer'
-         aria-label={alt} 
+         aria-label={alt}
        >
-         <Image
-           src={src}
-           alt={alt}
-           width={!fill ? width : undefined}
-           height={!fill ? width : undefined}
-           fill={fill}
-           sizes={sizes}
-           style= { {borderRadius, objectFit: 'cover', ...style }}
-           className={className}
-           tabIndex='0'
-         /> 
+         {commonImage}
        </a>
       ) : (
-        <Image
-        src={src}
-        alt={alt}
-        width={!fill ? width : undefined}
-        height={!fill ? width : undefined}
-        fill={fill}
-        sizes={sizes}
-        style= { {borderRadius, objectFit: 'cover', ...style }}
-        className={className}
-        tabIndex='0'
-      /> 
+        {commonImage}
       )}
-     
+
       {caption && (
         <figcaption className="text-white font-bold mt-2 text-[16px] text-center max-w-[200px] break-words leading-[1.4]">
           {link ? (
-            <a
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ textDecoration: 'none', color: 'inherit' }}
-            tabIndex='0'
-            >
-              {caption}
+              <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: 'none', color: 'inherit' }}
+              tabIndex='0'
+              >
+                {caption}
               </a>
-              ) : (
-                caption)}
+            ) : ({caption})
+          }
           </figcaption>
         )}
 
@@ -76,8 +68,6 @@ const CustomImage = ({
           {overlayText}
         </div>
       )}
-
-      
     </figure>
   );
 };
