bea75b5cbf15131b6f29431e3619a9f3d5ba37ca
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-21 10:20:13 +0000

    Refactor Render left/right columns using children[0] and children[1]

--- src/app/components/TwoColLayout.jsx
+++ src/app/components/TwoColLayout.jsx
@@ -11,10 +11,12 @@ const TwoColLayout=({
     return (
         <div className={` ${containerClass} bg-[#1C0080] flex mx-auto md:flex-row`}>
             <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-1 3xl:order-2">
-            {leftColumn || children}
+            {/* left Column */}
+            {children[0]}
             </div>
             <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:oder-2 3xl:order-1">
-            {rightColumn || children}
+            {/* right column */}
+            {children[1]}
             </div>
         </div>
     )
