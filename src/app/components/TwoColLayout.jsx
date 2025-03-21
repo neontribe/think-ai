4fefe019fcd56383cb48d9b4180dd7404aad5af3
charles strange <charles@neontribe.co.uk>
2025-03-21 17:11:01 +0000

    fix: ordering.

--- src/app/components/TwoColLayout.jsx
+++ src/app/components/TwoColLayout.jsx
@@ -6,15 +6,15 @@ const TwoColLayout=({
     children,
     leftColumn,
     rightColumn,
-    containerClass = "w-full min-h-screen p-6 md:p-12 flex flex-col md:flex-row flex-wrap",
+    containerClass = "w-full min-h-screen p-6 md:p-12 flex flex-col-reverse md:flex-row flex-wrap",
 }) => {
     return (
         <div className={` ${containerClass} bg-[#1C0080] mx-auto`}>
-            <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-1 3xl:order-2 flex-grow">
+            <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-2 3xl:order-2 flex-grow">
             {/* left Column */}
             {children[0]}
             </div>
-            <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-2 3xl:order-1 flex-grow">
+            <div className="w-full md:w-1/2 h-full text-white flex flex-col items-center justify-center md:order-1 3xl:order-1 flex-grow">
             {/* right column */}
             {children[1]}
             </div>
