7143489f187750c619dba54897e6468ea47e3263
charles strange <charles@neontribe.co.uk>
2025-03-21 16:33:01 +0000

    wip: add stack reversal flag

--- src/app/components/TwoColLayout.jsx
+++ src/app/components/TwoColLayout.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-const TwoColLayout = ({leftColumn, rightColumn, children}) => {
+const TwoColLayout = ({leftColumn, rightColumn, children, stackReverse = false}) => {
   // Combine left, children, and right into a single array
   const combinedContent = [
     ...(leftColumn ? [leftColumn] : []),
@@ -11,12 +11,14 @@ const TwoColLayout = ({leftColumn, rightColumn, children}) => {
   ];
 
   // Calculate the width based on the number of combined items
-  const widthClass = `w-full md:w-1/${combinedContent.length}`;
+  const widthClass = `md:w-1/${combinedContent.length}`;
+  const stackOrder = stackReverse ? 'flex-col-reverse' : 'flex-col';
 
-  return (<div className={`w-full h-auto flex flex-wrap mx-auto`}>
-      {combinedContent.map((content, index) => (<div
+  return (<div className={`w-full min-h-screen flex p-6 md:p-12 ${stackOrder} md:flex-row`}>
+      {combinedContent.map((content, index) => (
+        <div
           key={'key_'+index}
-          className={`${widthClass} h-full text-white flex flex-col items-center justify-center order-${index + 1}`}
+          className={`w-full ${widthClass} text-white flex flex-col items-center justify-center md:order-${index + 1} flex-grow`}
         >
           {content}
         </div>))}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,19 +4,20 @@ import Header from "./components/Header";
 import TwoColLayout from "@/app/components/TwoColLayout";
 import Image from "next/image";
 import SplashImage from "@/app/assets/SplashImage.png";
-import Link from "next/link";
 import RouteButton from "@/app/components/RouteButton";
 
 export default function Home() {
-  return (
-    <div className="full-width-container">
+  return (<div className="full-width-container">
       <Header/>
       <div className="constrained-width-container">
         <TwoColLayout>
-          <div className="">
-          <p className="text-5xl font-semibold leading-[1.5] pb-12">Learn <span className="text-[#87E2D9] md:ml-2">how to use generative AI</span> while you use it</p>
+          <div id="leftColumn">
+            <p className="text-4xl md:text-5xl font-semibold leading-[1.5] pb-12">
+              Learn <span className="text-[#87E2D9] md:ml-2">how to use generative AI</span> while you use it
+            </p>
 
-            <nav className="flex flex-col gap-2 md:flex-row md:gap-8 md:justify-between w-full" aria-label="Choose how to explore">
+            <nav className="flex flex-col gap-2 items-center md:flex-row md:gap-8 md:justify-between w-full"
+                 aria-label="Choose how to explore">
               <RouteButton
                 ariaLabel="generate an image"
                 href="/process/query?modelType=image-generation"
@@ -31,11 +32,10 @@ export default function Home() {
               </RouteButton>
             </nav>
           </div>
-          <div>
+          <div id="rightColumn">
             <Image src={SplashImage} alt=''/>
           </div>
         </TwoColLayout>
       </div>
-    </div>
-  );
+    </div>);
 }
