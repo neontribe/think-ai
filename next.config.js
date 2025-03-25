58de5d3538e384e2b966184f6ce4f651d79d2653
charles strange <charles@neontribe.co.uk>
2025-03-25 11:58:18 +0000

    fix: use a .js file not an .mjs

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.blob.core.windows.net',
+        pathname: '/**',
+      },
+    ],
+  },
+}
