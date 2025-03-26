a8c36b84cdbfafacbbfd45c47c5f0da519861057
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-03-26 17:53:28 +0000

    Feat: Add summary-text/page.jsx in discussion folder

---
+++ src/app/data/summaryResultData.js
@@ -0,0 +1,32 @@
+const summaryResultData = [
+  {
+    src: "",
+    alt: "",
+    caption: "",
+    link: "",
+    buttonText: "",
+  },
+  {
+    src: "",
+    alt: "",
+    caption: "",
+    link: "",
+    buttonText: "",
+  },
+  {
+    src: "",
+    alt: "",
+    caption: "",
+    link: "",
+    buttonText: "",
+  },
+  {
+    src: "",
+    alt: "",
+    caption: "",
+    link: "",
+    buttonText: "",
+  },
+];
+
+export default summaryResultData;
