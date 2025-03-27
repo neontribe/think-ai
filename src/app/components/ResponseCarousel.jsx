b00e9095f661ba94a82bf1076e573272d6149fb7
charles strange <charles@neontribe.co.uk>
2025-03-27 15:41:28 +0000

    fix: new slider approach

--- src/app/components/ResponseCarousel.jsx
+++ src/app/components/ResponseCarousel.jsx
@@ -30,19 +30,19 @@ const ResponseCarousel = ({slideData = []}) => {
 
   return (
     <div
-      className="relative w-full max-w-4xl md:h-80 lg:h-[28rem] bg-[#291498] rounded-3xl shadow-lg overflow-hidden mx-auto">
+      className="relative overflow-hidden w-full max-w-4xl mx-auto rounded-3xl shadow-lg bg-[#291498]">
       {/* slider container */}
-      <div className="flex flex-col items-start space-y-4 transition-transform duration-500 ease-in-out"
+      <div className="flex transition-transform duration-700 ease-in-out"
+           style={{transform: `translateX(-${(currentIndex * 100)}%`}}
       >
         {localSlides.map((slide, index) => {
           return (<div
             key={'key_'+index}
-            className="absolute inset-0 flex items-center justify-center text-white transition-transform duration-700 ease-in-out"
-            style={{transform: `translateX(${(index - currentIndex) * 100}%)`}}
+            className="w-full shrink-0 p-8 text-white"
           >
 
             {/* slide content */}
-            <div className="flex items-start self-start -mt-2 p-14 mr-4 rounded-3xl max-w-[90%]">
+            <div className="flex self-start -mt-2 p-14 mr-4 rounded-3xl max-w-[90%]">
               <div className="text-8xl font-bold relative mt-4 mr-4">{slide.number}.</div>
               <div className="flex-1">
                 <h1 className="text-lg">{slide.paragraph}</h1>
@@ -50,18 +50,17 @@ const ResponseCarousel = ({slideData = []}) => {
             </div>
           </div>)
         })}
-
+      </div>
         <div
           className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full max-w-[90%] px-4 z-20">
-          <div className={currentIndex === 0 ? "invisible" : ""}>
+          <div className={currentIndex === 0 ? "invisible" : "" }>
             <ArrowButton direction="left" onClick={nextSlide} ariaLabel="Next slide"/>
           </div>
-          <div className={currentIndex !== 0 ? "invisible" : ""}>
+          <div className={currentIndex === slideData.length -1 ? "invisible" : "" }>
             <ArrowButton direction="right" onClick={prevSlide} arialLabel="Previous slide"
                          className={currentIndex === slideData.length - 1 ? "hidden" : ""}/>
           </div>
         </div>
-      </div>
     </div>);
 
 };
--- src/app/process/discuss/image-generation/page.jsx
+++ src/app/process/discuss/image-generation/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useGlobalState } from "@/app/contexts/GlobalStateProvider";
+import {useGlobalState} from "@/app/contexts/GlobalStateProvider";
 import ProcessGuard from "@/app/components/ProcessGuard";
 import ThreeRowLayout from "@/app/components/ThreeRowLayout";
 import Header from '@/app/components/Header';
@@ -12,54 +11,47 @@ import Star from "@/app/assets/Star.png"
 import LearnBox from '@/app/components/LearnBox';
 import imageResultData from '@/app/data/imageResultData'
 
-  const Discuss = () => {
-    const router = useRouter();
-    const { state } = useGlobalState();
-    const { responseData } = state;
-    const { modelType, promptResponseContent, splitRiskPoints } = responseData;
+const Discuss = () => {
+  const {state} = useGlobalState();
+  const {responseData} = state;
+  const {modelType, promptResponseContent, splitRiskPoints} = responseData;
 
   const slides = splitRiskPoints.filter(Boolean).map((risk, index) => ({
-    number: index + 1,
-    paragraph: risk,
+    number: index + 1, paragraph: risk,
   }));
 
-  
 
-  return (
-    <>
-      <Header />
+  return (<>
+      <Header/>
       <ThreeRowLayout
-        top={
-          <div className="flex flex-col gap-6">
-            <h2 className="text-[16pt] gap-4 font-bold md:ml-10">
-              Let’s Dive Deeper Into This Image
-            </h2>
-        
-            {/* LEFT*/}
-            <div className="relative flex flex-col md:flex-row">
-              <div className="md:w-1/2 relative z-20 md:w-[80%] md:-mr-30">
-                <ResponseCarousel slideData={slides} />
-              </div>
-        
-              {/* RIGHT */}
-              <div className="md:w-1/2 flex md:justify-start md:-mt-8">
-                {modelType === 'image-generation' && promptResponseContent && (
-                  <Image
-                    src={promptResponseContent}
-                    alt="Requested asset"
-                    width={379}
-                    height={379}
-                    className="rounded-2xl shadow-md"
-                  />
-                )}
-              </div>
+        top={<div className="flex flex-col gap-6">
+          <h2 className="text-[16pt] font-bold md:ml-10">
+            Let’s Dive Deeper Into This Image
+          </h2>
+
+          {/* LEFT*/}
+          <div className="flex flex-col md:flex-row">
+            <div className="z-20">
+              <ResponseCarousel slideData={slides}/>
             </div>
+
+            {/* RIGHT */}
+            {/*
+            <div className="md:w-1/2 flex md:justify-start md:-mt-8">
+              {modelType === 'image-generation' && promptResponseContent && (<Image
+                  src={promptResponseContent}
+                  alt="Requested asset"
+                  width={379}
+                  height={379}
+                  className="rounded-2xl shadow-md"
+                />)}
+            </div>
+            */}
           </div>
-        }
-        
-        middle={
-          <div className="gap-4 w-full flex flex-col md:flex-row items-center">
-            <div className="flex flex-col gap-4 w-full md:w-3/4 md:ml-8">
+        </div>}
+
+        middle={<div className="gap-4 w-full flex flex-col md:flex-row items-center">
+          <div className="flex flex-col gap-4 w-full md:w-3/4 md:ml-8">
             <h2 className="text-[16pt] font-bold mb-4">
               Take some time to reflect
             </h2>
@@ -74,66 +66,70 @@ import imageResultData from '@/app/data/imageResultData'
               <div className="mt-2 px-4 pb-4 space-y-2">
                 <p>1. You might jump into learning more about how it works, check out the guides below.</p>
                 <p>2. You might want to experiment with making images and trying to get more fair results.</p>
-                <p>If you feel confident on how to generate images and what to be aware of, check out other tools like 
-                  <a 
-                  href="https://www.adobe.com/uk/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[#65E1F1] hover:underline"
-                  > Adobe Firefly</a>, 
-                  or {' '} <a 
-                  hrfe="https://www.midjourney.com/home"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[#65E1F1] hover:underline"
+                <p>If you feel confident on how to generate images and what to be aware of, check out other tools like
+                  <a
+                    href="https://www.adobe.com/uk/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-[#65E1F1] hover:underline"
+                  > Adobe Firefly</a>,
+                  or {' '} <a
+                    hrfe="https://www.midjourney.com/home"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-[#65E1F1] hover:underline"
                   > Mid Journey</a>{' '}
                   for more advanced features.</p>
-                <p>3. You could leave it there with images and learn more about another type of generative AI with us, like text generation.</p>
+                <p>3. You could leave it there with images and learn more about another type of generative AI with us,
+                  like text generation.</p>
               </div>
             </ExpandAccordion>
 
             <ExpandAccordion text="If you do make images with ai, try following these points">
               <div className="mt-2 px-4 pb-4 space-y-2">
                 <p>1. Make sure you describe the features and layout of your image clearly and in detail.</p>
-                <p>2. Be aware of the fact that the AI will already have ideas about the type of people, things or places you want to make from the images it’s scanned before. For example, ai might produce more images of doctors that are white middle age men than any other appearance, because so are most images it’s seen of doctors before.</p>
-                <p>3. If you don’t want ai to unfairly weight the image result towards what it’s learnt before, then be clear about that in your description.</p>
-                <p>4. Be aware of copyright, and that styling things to be like another artist could feel like stealing their creative ideas. Think for yourself and do your research about where that line is. Some tools won’t let you style work based on another artist, for example.</p>
-                <p>5. Remember image generation tools are still growing and policies are still changing as the tech develops. They aren’t perfect, and you can check out the use policies and regulations of the tool you are using if you are unsure.</p>
+                <p>2. Be aware of the fact that the AI will already have ideas about the type of people, things or
+                  places you want to make from the images it’s scanned before. For example, ai might produce more images
+                  of doctors that are white middle age men than any other appearance, because so are most images it’s
+                  seen of doctors before.</p>
+                <p>3. If you don’t want ai to unfairly weight the image result towards what it’s learnt before, then be
+                  clear about that in your description.</p>
+                <p>4. Be aware of copyright, and that styling things to be like another artist could feel like stealing
+                  their creative ideas. Think for yourself and do your research about where that line is. Some tools
+                  won’t let you style work based on another artist, for example.</p>
+                <p>5. Remember image generation tools are still growing and policies are still changing as the tech
+                  develops. They aren’t perfect, and you can check out the use policies and regulations of the tool you
+                  are using if you are unsure.</p>
               </div>
-            </ExpandAccordion> 
-            </div>
-
-            <div className="flex flex-col md:w-1/3 items-center mt-4 md:mt-0 md:ml-10">
-            <Image
-            src={Star}
-            alt="Decorative star"
-            className="w-[86px] h-[94px] md:w-[177px] md:h-[193px]"
-            />
-            </div>
+            </ExpandAccordion>
           </div>
-        }
-        bottom={
-        <div className="w-full mx-auto px-2">
+
+          <div className="flex flex-col md:w-1/3 items-center mt-4 md:mt-0 md:ml-10">
+            <Image
+              src={Star}
+              alt="Decorative star"
+              className="w-[86px] h-[94px] md:w-[177px] md:h-[193px]"
+            />
+          </div>
+        </div>}
+        bottom={<div className="w-full mx-auto px-2">
           <h2 className="text-[16pt] font-bold md:ml-10">
             Go further and learn more about making images with AI
-            </h2>
-            <div className="flex flex-col md:flex-row flex-wrap md:ml-10 gap-8 mt-16 md:space-y-6 sm:space-y-6 items-start">
-            {imageResultData.map((item, index) => (
-              <LearnBox
-              key={index}
-              src={item.src}
-              alt={item.alt}
-              caption={item.caption}
-              link={item.link}
-              buttonText={item.buttonText}
-              />
-              ))}
-            </div>
+          </h2>
+          <div
+            className="flex flex-col md:flex-row flex-wrap md:ml-10 gap-8 mt-16 md:space-y-6 sm:space-y-6 items-start">
+            {imageResultData.map((item, index) => (<LearnBox
+                key={index}
+                src={item.src}
+                alt={item.alt}
+                caption={item.caption}
+                link={item.link}
+                buttonText={item.buttonText}
+              />))}
           </div>
-        }
+        </div>}
       />
-    </>
-  );
+    </>);
 };
 
 export default ProcessGuard(Discuss);
