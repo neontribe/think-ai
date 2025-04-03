bdd16a21599b6d11482e915405cb673ba0b94c89
charles strange <charles@neontribe.co.uk>
2025-04-03 11:26:10 +0100

    feat: add prettier and customise eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@
 module.exports = {
   root: true,
   extends: [
-    'next/core-web-vitals',
+    'next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'
   ],
-  rules: {},
+  rules: {
+    // not used in react 17+
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off"
+  },
 };
